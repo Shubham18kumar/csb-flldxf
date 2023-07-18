ef3c5b87a6e8af0a6433ffcbd821654ffa73e17b
Code Sandbox <hello@codesandbox.io>
2023-07-18 22:10:45 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.f4cc75f4ac0f0bccf4b8950728dffad1.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7e3e68f9b06f8c801600af950fe9197",
+    "url": "https://shubham18kumar.github.io/csb-flldxf/index.html"
+  },
+  {
+    "revision": "333ae1545be604ef8ace",
+    "url": "https://shubham18kumar.github.io/csb-flldxf/static/js/main.914fe783.chunk.js"
+  },
+  {
+    "revision": "cae99df622d909609ac3",
+    "url": "https://shubham18kumar.github.io/csb-flldxf/static/js/runtime-main.01546019.js"
+  }
+]);
